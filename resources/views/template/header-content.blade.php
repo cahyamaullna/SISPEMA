0d64833a1ab42cbaa03ac037ea7857ee49289a2c
cahyaah66 <cahyaah66@gmail.com>
2023-02-23 12:58:12 +0700

    push

--- resources/views/template/header-content.blade.php
+++ resources/views/template/header-content.blade.php
@@ -4,7 +4,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="{{ url('/main') }}" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">Ajuk La</span>
+        <span class="d-none d-lg-block">SISPEMA</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
@@ -20,7 +20,7 @@
 
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="{{ asset('storage/profile_image_file/'. Auth::user()->foto_profile) }}" alt="Profile" class="rounded-circle">
+            <img src="/img/default-profile.jpg" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">Hi , {{ Auth::user()->name }}</span>
           </a><!-- End Profile Iamge Icon -->
 
